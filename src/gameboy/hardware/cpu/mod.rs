f591dd3bde2441da8e145be857f75d126a755be6
Daniel Ribeiro <drgomesp@gmail.com>
2018-04-30 11:28:41 +0400

    Created basic CPU struct with InstructionCycle trait impl

--- src/gameboy/hardware/cpu/mod.rs
+++ src/gameboy/hardware/cpu/mod.rs
@@ -1,1 +1,28 @@
 mod instructions;
+
+use self::instructions::*;
+use self::instructions::Instruction::*;
+
+pub struct Cpu {}
+
+impl Cpu {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl InstructionCycle for Cpu {
+    fn fetch(&self, _addr: u16) -> u8 {
+        0
+    }
+
+    fn decode(&self) -> Instruction {
+        NOP {
+            info: InstructionInfo::default(),
+        }
+    }
+
+    fn execute(&self) -> usize {
+        0
+    }
+}
